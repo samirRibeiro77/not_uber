86c6444e00c8f47bceafff1468179cc06e3188b7
Samir Ribeiro <samir.sjr77@gmail.com>
2025-07-01 16:28:38 -0300

    Maps
    Passenger map

--- lib/src/ui/home_page/passenger_home_page.dart
+++ lib/src/ui/home_page/passenger_home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:not_uber/src/ui/home_page/home_appbar.dart';
 
 class PassengerHomePage extends StatefulWidget {
@@ -9,11 +12,23 @@ class PassengerHomePage extends StatefulWidget {
 }
 
 class _PassengerHomePageState extends State<PassengerHomePage> {
+  final _mapController = Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
     print(kToolbarHeight);
     return Scaffold(
-      appBar: HomeAppbar(title: "Passenger")
+      appBar: HomeAppbar(title: "Passenger"),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-22.92584646807185, -43.17913837568963),
+          zoom: 16
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+      ),
     );
   }
 }
